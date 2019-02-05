1870e4cde80bf4e04a3e4ef4228be7a9d2f27b9c
Clément BOURGES <clbourges1@local.isima.fr>
2019-02-05 19:19:26 +0100

    First commit

---
+++ src/main/java/isima/reversi/ReversiApplication.java
@@ -0,0 +1,14 @@
+package isima.reversi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReversiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReversiApplication.class, args);
+	}
+
+}
+
